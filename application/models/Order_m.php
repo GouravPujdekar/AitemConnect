a27c92f832868c5f309458f1db49a1ad65405dc5
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-11 20:21:17 +0530

    Add files via upload

---
+++ application/models/Order_m.php
@@ -0,0 +1,26 @@
+<?php
+class Order_m extends CI_Model
+ {
+   function getAllOrders() {   
+      /* API URL */
+      $postData = json_decode(file_get_contents('php://input'));
+      //$firstName = $postData->firstName;   
+      $token=$this->session->userdata('authToken');  
+      $t="K5WkApK4pGBazFpXZSz8HrcCNcjPXU8rvURxqCbr3Vj7TcCOY8r6xt5NCzmV+HT1lio/BKKzHLVH3f3LgsrVDg==";
+      $url = 'http://18.188.222.175:8080/orders';   
+      /* Init cURL resource */
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, 'http://18.188.222.175:8080/orders');
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($ch, CURLOPT_GET, true);
+      //curl_setopt($ch, CURLOPT_POSTFIELDS,$d);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('api-key-token:'.$t,'Content-Type: application/json', 'Accept: application/json'));
+      $out = curl_exec($ch);
+         if ($out === false) {
+         echo 'Curl error : ' . curl_error($ch);
+         }   
+      curl_close($ch);      
+      return $out;    
+      }
+    }
+?>
