0f8553d4254107bfa9608486965239e4e0ad659e
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-11 20:19:15 +0530

    Add files via upload

---
+++ assets/angular/angularjs/controller/order.js
@@ -0,0 +1,36 @@
+app.controller('orderCtrl', function($http, $scope, toastr){
+    $scope.init = function() {       
+            $scope.getAllOrderDetails();      
+            localStorage.clear();  
+            $scope.loader=false;            
+        }
+        
+    $scope.getAllOrderDetails = function() {       
+       
+        $scope.loader=true; 
+        $http.get(window.site_url + 'Order/getAllOrders').
+        then((response) => {
+            $scope.loader=false;  
+            $scope.orderListt = response.data;
+            //console.log($scope.shopList);
+            if(response.data['status']=="400"){
+                $scope.loader=false;  
+                toastr.warning("Something went wrong!");
+            }else if(response.data['status']=="500"){
+                $scope.loader=false;  
+                toastr.warning("Something went wrong!");
+            }
+            else if(response.data==0){
+                toastr.warning("Orders are not available");
+            }else{           
+                $scope.loader=false;        
+                $scope.orderList = $scope.orderListt;
+                console.log($scope.orderList);
+            }
+        },function(err){
+             console.log(err);
+            // $scope.init();
+             $scope.loader=false;
+     });        
+    }     
+    });
