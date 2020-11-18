e6e6892ef2ca7e9e696a09f55c4f0526dee144dd
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-18 08:54:51 +0530

    Update app.js

--- assets/angular/angularjs/app.js
+++ assets/angular/angularjs/app.js
@@ -1,5 +1,5 @@
 
 var app = angular.module('myApp',['ui.bootstrap', 'toastr']);
-var site_url = 'http://3.131.82.166/AitemConnect/index.php/';
+var site_url = 'http://3.139.65.132/AitemConnect/index.php/';
 
 
