a93543f154bb9164bea6c9e2052d58541a55bf51
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-19 17:24:49 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://3.139.65.132/AitemConnect/';
+$config['base_url'] = 'http://3.139.65.132/';
 
 /*
 |--------------------------------------------------------------------------
