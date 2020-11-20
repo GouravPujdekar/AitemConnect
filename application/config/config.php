87078175dcba52e98f357606534cadba02b5ff71
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-20 12:55:02 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = 'http://3.139.65.132/';
 $root="http://".$_SERVER['HTTP_HOST'];
 $root.=str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-//$config['base_url'] = 'http://algostatic.in/Payment/';
+//$config['base_url'] = '';
 $config['base_url'] ="$root";
 /*
 |--------------------------------------------------------------------------
