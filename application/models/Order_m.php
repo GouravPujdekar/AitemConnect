3f922702465fe8f745f848bff54cff187d400683
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-20 12:33:22 +0530

    Update Order_m.php

--- application/models/Order_m.php
+++ application/models/Order_m.php
@@ -7,11 +7,11 @@ class Order_m extends CI_Model
       //$firstName = $postData->firstName;   
       $token=$this->session->userdata('authToken');  
       $t="K5WkApK4pGBazFpXZSz8HrcCNcjPXU8rvURxqCbr3Vj7TcCOY8r6xt5NCzmV+HT1lio/BKKzHLVH3f3LgsrVDg==";
-     $root="http://".$_SERVER['HTTP_HOST']."/";     
+     $root="http://".$_SERVER['HTTP_HOST'].":8080";     
       $url ="$root";  
       /* Init cURL resource */
       $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url.':8080/orders');
+      curl_setopt($ch, CURLOPT_URL, $url.'/orders');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       // curl_setopt($ch, CURLOPT_GET, true);
       //curl_setopt($ch, CURLOPT_POSTFIELDS,$d);
