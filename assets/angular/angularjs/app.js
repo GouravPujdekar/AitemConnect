86cfe0b27232e649b3cdf00b2d5e2d76a2ab78b5
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-20 11:28:26 +0530

    Update app.js

--- assets/angular/angularjs/app.js
+++ assets/angular/angularjs/app.js
@@ -1,5 +1,6 @@
 
 var app = angular.module('myApp',['ui.bootstrap', 'toastr']);
-var site_url = 'http://3.139.65.132/index.php/';
-
+//var site_url = 'http://3.139.65.132/index.php/';
+var url=window.location;
+var site_url=url;
 
