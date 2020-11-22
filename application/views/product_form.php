6d9a561fd4b1b289a3fc3257c0f6d1b1ebefacdc
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-22 13:05:01 +0530

    Update product_form.php

--- application/views/product_form.php
+++ application/views/product_form.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Stripe Gateway Integration | Codeigniter</title>
+	<title>Stripe Gateway </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
