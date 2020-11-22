b0f56a52ac08ac30f35905ec982f59d4cf14fabb
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-22 13:06:43 +0530

    Update payment_success.php

--- application/views/payment_success.php
+++ application/views/payment_success.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
     
-	<title>Stripe Gateway Integration | Codeigniter</title>
+	<title>Stripe Gateway </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
