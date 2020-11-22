897c5266076874117d33a0148494e8ceecad3c9c
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-22 12:10:07 +0530

    Update Order_m.php

--- application/models/Order_m.php
+++ application/models/Order_m.php
@@ -8,7 +8,7 @@ class Order_m extends CI_Model
       $token=$this->session->userdata('authToken');  
       $t="K5WkApK4pGBazFpXZSz8HrcCNcjPXU8rvURxqCbr3Vj7TcCOY8r6xt5NCzmV+HT1lio/BKKzHLVH3f3LgsrVDg==";
      $root="http://".$_SERVER['HTTP_HOST'].":8080";   
-    $url="http://3.139.65.132:8080/";
+    $url="http://3.139.65.132:8080";
       $urll ="$root";  
       /* Init cURL resource */
       $ch = curl_init();
