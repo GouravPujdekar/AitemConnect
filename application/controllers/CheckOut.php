184ddb993ac034a4a32d79c526ff2e7a30be8c86
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-11-22 12:49:38 +0530

    Update CheckOut.php

--- application/controllers/CheckOut.php
+++ application/controllers/CheckOut.php
@@ -22,6 +22,7 @@ class CheckOut extends CI_Controller {
 			$card_exp_year = $_POST['exp_year'];
 			$amount=$_POST['amount'];
 			$itemId=$_POST['id'];
+			$orderId=$_POST['orderId'];
 			$quantity=$_POST['quantity'];
 			//include Stripe PHP library
 			require_once APPPATH."third_party/stripe/init.php";
@@ -45,7 +46,7 @@ class CheckOut extends CI_Controller {
 			$itemNumber = $itemId;
 			$itemPrice = $amount;
 			$currency = "usd";
-			$orderID = "SKA92712382139";
+			$orderID = $orderId;
 			
 			//charge a credit or a debit card
 			$charge = \Stripe\Charge::create(array(
@@ -78,30 +79,17 @@ class CheckOut extends CI_Controller {
 					'quantity'=>$quantity,						 
 				));								
 			
-				//insert tansaction data into the database
-				/*$dataDB = array(
-					'name' => $name,
-					'email' => $email, 
-					'card_num' => $card_num, 
-					'card_cvc' => $card_cvc, 
-					'card_exp_month' => $card_exp_month, 
-					'card_exp_year' => $card_exp_year, 
-					'item_name' => $itemName, 
-					'item_number' => $itemNumber, 
-					'item_price' => $itemPrice, 
-					'item_price_currency' => $currency, 
-					'paid_amount' => $amount, 
-					'paid_amount_currency' => $currency, 
-					'txn_id' => $balance_transaction, 
-					'payment_status' => $status,
-					'created' => $date,
-					'modified' => $date
-				);*/
+				$statusArray =array(										
+				
+					'orderId'=>$orderId,
+					'orderStatus'=>"CHECKED_OUT",						 
+				);
+
 				$token=$this->session->userdata('authToken');  
 				$t="K5WkApK4pGBazFpXZSz8HrcCNcjPXU8rvURxqCbr3Vj7TcCOY8r6xt5NCzmV+HT1lio/BKKzHLVH3f3LgsrVDg==";
 				$root="http://".$_SERVER['HTTP_HOST'].":8080";     
 				      $urll ="$root"; 
-				$url="http://3.139.65.132:8080/";
+				$url="http://3.139.65.132:8080";
 				$dd=json_encode($d);
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, $url.'/orders');
@@ -114,6 +102,22 @@ class CheckOut extends CI_Controller {
 				   echo 'Curl error : ' . curl_error($ch);
 				   }   
 				curl_close($ch);  
+				
+				$ddd=json_encode($statusArray);				
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, 'http://3.139.65.132:8080/orders/'.$orderId);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+				curl_setopt($ch, CURLOPT_POSTFIELDS,$ddd);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array('api-key-token:'.$token,'Content-Type: application/json', 					'Accept: application/json'));
+				$out = curl_exec($ch);
+				   if ($out === false) {
+				   echo 'Curl error : ' . curl_error($ch);
+				   }   
+				curl_close($ch);  
+						
+				
+				
 				$this->load->view('payment_success', $dd);
 				return $out;
 			}else{
