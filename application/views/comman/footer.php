73ac9d060d6f72dfb46c89cdb3e2a4191ba7e9d8
vgworiox <73926315+vgworiox@users.noreply.github.com>
2020-12-15 16:03:15 +0530

    Add files via upload

--- application/views/comman/footer.php
+++ application/views/comman/footer.php
@@ -6,97 +6,79 @@
                         <div class="footer-widget footer-about">
                             <div class="footer-logo">
                                 <a href="index.html">
-                                    <img src="assets/images/logo/footer-logo.png" alt="logo">
+                                    <img src="<?php echo base_url('assets/images/HopTologo.jpg');?>" alt="logo">
                                 </a>
                             </div>
-                            <p>Lorem ipsum dolor sit amet, porta feugiat odio nam ut magnis tempor. Vitae quis nisl viverra adipiscing in, integer penatibus elit luctus </p>
+                            
                             <ul>
                                 <li>
                                     <a href="tel:80930458459">
                                         <!-- <i class="fas fa-phone"></i> -->
                                         <i class="flaticon-telephone-handle-silhouette"></i>
-                                        9999 3333 8888
-                                    </a>
+                                        847-981-5165                                    </a>
                                 </li>
                                 <li>
                                     <a href="#0">
                                         <i class="flaticon-maps-and-flags"></i>
                                         <!-- <i class="fas fa-map-marker-alt"></i>  -->
-                                        Minnie Downs QLD 4478
-                                    </a>
+                                      HopTo Inc.
+1600 Golf Road Suite 1200,
+Rolling Meadows, IL. 60008
+Office - 847-981-5165.                                    </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-4">
+                    <div class="col-md-4 col-sm-4">
                         <div class="footer-widget link-widget pl-lg-4">
-                            <h4 class="title">Our Branches</h4>
+                          
                             <ul>
                                 <li>
-                                    <a href="#0">Alberta</a>
+                                    <a href="https://aitemconnect.com/Driver%20Safety%20Tips.pdf" target="_blank">Driver Safety Tips</a>
                                 </li>
                                 <li>
-                                    <a href="#0">Columbia</a>
+                                    <a href="https://aitemconnect.com/Driver%20Independent%20Contractor%20Agreement%20final.pdf"  target="_blank">Independent Contractor Agreement</a>
                                 </li>
                                 <li>
-                                    <a href="#0">Manitoba</a>
+                                    <a href="https://aitemconnect.com/Privacy%20Policy.pdf" target="_blank">Privacy Policy</a>
                                 </li>
                                 <li>
-                                    <a href="#0">Nunavut</a>
+                                    <a href="https://aitemconnect.com/Terms%20of%20Use.pdf" target="_blank">Terms of Use</a>
                                 </li>
                                 <li>
-                                    <a href="#0">Yukon</a>
+                                    <a href="https://aitemconnect.com/Deactivation%20policy%20-%20AitemConnect-c.pdf" target="_blank">Deactivation policy</a>
                                 </li>
-                                <li>
-                                    <a href="#0">Onturio</a>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
-                    <div class="col-md-5">
-                        <div class="footer-widget footer-schedule">
-                            <h4 class="title">Opening Hours</h4>
+
+  <div class="col-md-2 col-sm-2">
+                        <div class="footer-widget link-widget pl-lg-4">
+                          
                             <ul>
                                 <li>
-                                    <a href="#0">
-                                        <span>Sat-Tues</span>
-                                        <span>08:00am-01:00pm</span>
-                                    </a>
+                                    <a href="<?php echo site_url("Home/Login");?>">Login</a>
                                 </li>
-                                <li>
-                                    <a href="#0">
-                                        <span>Wed-Thurs</span>
-                                        <span>12:00am-03:00pm</span>
-                                    </a>
+                               <li>
+                                    <a href="<?php echo site_url("Home/Register");?>">Register</a>
                                 </li>
-                                <li>
-                                    <a href="#0">
-                                        <span>Friday</span>
-                                        <span>07:00am-09:00pm</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#0">
-                                        <span>Sunday</span>
-                                        <span>08:00am-01:00pm</span>
-                                    </a>
-                                </li>
+
+                               
+                                
                             </ul>
-                            <div class="text-right calendar">
-                                <a href="#0">Go to Calendar</a>
-                            </div>
                         </div>
                     </div>
+
+
+
+                   
                 </div>
             </div>
         </div>
-        <div class="footer-bottom py-4 bg-theme text-center">
-            <div class="container">
-                <p class="m-0"><a href="templateshub.net">Templateshub</a></p>
-            </div>
-        </div>
+       
     </footer>
-    <!-- ==========footer-section========== -->
+    <!-- ==========footer-section========== 
 
 
     <script src="assets/js/jquery-3.3.1.min.js"></script>
@@ -113,3 +95,4 @@
     <script src="assets/js/viewport.jquery.js"></script>
     <script src="assets/js/nice-select.js"></script>
     <script src="assets/js/main.js"></script>
+-->
--- application/views/comman/header.php
+++ application/views/comman/header.php
@@ -11,18 +11,15 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/flaticon.css');?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/lightcase.css');?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/odometer.css');?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/swiper.min.css');?>">
-    
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/swiper.min.css');?>">    
     <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css');?>">
-    <link rel="shortcut icon" href="<?php echo base_url('assets/images/thumbnail.png');?>" type="image/x-icon">
- 
-
+    <link rel="shortcut icon" href="<?php echo base_url('assets/images/HopTologo.jpg');?>" type="image/x-icon">
     <script src="<?php echo base_url('assets/angular/angular.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/angular/ui-bootstrap-tpls-1.0.3.js'); ?>"></script>
     <!-- toastr -->
     <script src="<?php echo base_url('assets/angular/angular-toastr.tpls.js'); ?>"></script>
     <link rel="stylesheet" href="<?php echo base_url('assets/angular/angular-toastr.css'); ?>" />
-    <title>AitemConnect</title>
+    <title>HopTo | <?php if(isset($title)) echo $title; ?></title>
 </head>
 <body>   
     <!-- ==========header-section========== -->
@@ -32,38 +29,40 @@
                 <div class="header-wrapper">
                     <div class="logo">
                         <a href="<?php echo site_url("Home");?>" class="logo">
-                         <img src="<?php echo base_url('assets/images/thumbnail.png');?>" style="height:60px;width:80px;"><b></b>
+                         <img src="<?php echo base_url('assets/images/HopTologo.png');?>" alt="HopTo"><b></b>
                         </a>
                     </div>
-                      <ul class="menu ml-auto">                       
-                       
-                    </ul>
-                   
-                    <ul class="search-area">
-                        <li>
-                            <?php
-                                $username=  $this->session->userdata('username'); 
-                                if(isset($username)){ 
+                      <ul class="menu ml-auto">                    
+                    </ul>                   
+                     <ul class="search-area">                       
+                               <?php
+                                $authToken=$this->session->userdata('username'); 
+				$profile=$this->session->userdata('role');
+                                if(isset($authToken) && $profile=="SHOPPER"){ 
                                   ?>
+				<li>
                             <a id="cart-button" href="<?php echo site_url("Home/Cart");?>" >
                                 <i class="flaticon-shopping-cart" style="margin-top:5px;"></i>
                             </a>
-                             <?php }?>
-                        </li>
-                          <li >
-                              <?php
-                                $username=  $this->session->userdata('username'); 
-                                if(isset($username)){ 
-                                  ?>
-                            <a href="<?php echo site_url("Logout");?>" style="margin-left:-8px;">
+				</li>
+				<li>
+			   <a href="<?php echo site_url("Logout");?>" style="margin-left:-8px;">
                             <i class="fa fa-power-off"></i>Logout
+                            </a> 
+				</li>
+                         <?php } else if(isset($authToken) && $profile=="RETAILER"){ ?>
+                            <li>
+				   <a href="<?php echo site_url("Logout");?>" style="margin-left:-8px;">
+                            <i class="fa fa-power-off"></i>Logout
+                            </a> 
+				</li>
+                            <?php } else{ ?>
+				<li>
+ 				<a href="<?php echo site_url("Home/Login");?>" style="margin-left:-8px;">
+                             Login
                             </a>
-                             <?php } else {?>
-                              <a href="<?php echo site_url("Home/Login");?>" style="margin-left:-8px;">
-                            Login
-                            </a>
-                              <?php } ?>
-                        </li>
+				</li>
+                         <?php }?>                      
                     </ul>
                 </div>
             </div>
